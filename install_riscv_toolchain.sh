a21532790ed9f252b38b5d534ae1743376f607af
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-08-09 11:34:18 +0000

    Change to 32-bit risc-v for soft-float support.

--- install_riscv_toolchain.sh
+++ install_riscv_toolchain.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -f riscv-toolchain-ubuntu-16.04.tar.gz ] ; then
-    BOXOBS=hpeizds5xe5bfiiw5n7j2173krwq3p57
+if [ ! -f riscv-toolchain-32-ubuntu-16.04.tar.gz ] ; then
+    BOXOBS=4lyu9meltq6fjfbbb6l2ob3qvc0xr2at
     wget https://imperialcollegelondon.box.com/shared/static/${BOXOBS}.gz
-    mv ${BOXOBS}.gz riscv-toolchain-ubuntu-16.04.tar.gz
+    mv ${BOXOBS}.gz riscv-toolchain-32-ubuntu-16.04.tar.gz
 fi
 
-if [ ! -d riscv-toolchain ] ; then
-    tar -xzf riscv-toolchain-ubuntu-16.04.tar.gz
+if [ ! -d riscv-toolchain-32 ] ; then
+    tar -xzf riscv-toolchain-32-ubuntu-16.04.tar.gz
 fi
 
 grep DT10_RISCV_TOOLCHAIN_ROOT ~/.bashrc
 if [ $? -ne 0 ] ; then
-    echo "export DT10_RISCV_TOOLCHAIN_ROOT=$(pwd)/riscv-toolchain/" >> ~/.bashrc
+    echo "export DT10_RISCV_TOOLCHAIN_ROOT=$(pwd)/riscv-toolchain-32/" >> ~/.bashrc
     echo 'export PATH=${PATH}:${DT10_RISCV_TOOLCHAIN_ROOT}/bin' >> ~/.bashrc
 fi
