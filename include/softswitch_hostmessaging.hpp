212ff0d6eac7ba6980049af63a1e0ac1dba6b199
Shane Fleming <sf306@aesop.cl.cam.ac.uk>
2018-04-18 20:48:38 +0100

    Added the flags to enable logging by level again. We now only run out of memory when all logging is turned up to max.

--- include/softswitch_hostmessaging.hpp
+++ include/softswitch_hostmessaging.hpp
@@ -9,6 +9,7 @@
 //#include "softswitch_hostmessaging.hpp"
 #include "tinsel_api.hpp"
 #include <stdint.h>
+#include <vector>
 #include "hostMsg.hpp"
 #include "softswitch.hpp"
 
@@ -73,7 +74,6 @@ void log_peel_params(hostMsg *hmsg, uint32_t* cnt) {
 
 // peel parameters
 // General case: peels the parameters off the argument list and appends them to the message
-// TODO: change this so that it uses initialisers to consume less code space
 template<typename P1, typename ... Param>
 void log_peel_params(hostMsg *hmsg, uint32_t* cnt, const P1& p1, Param& ... param){
     hmsg->payload[*cnt] = *((uint32_t*)&p1); //prune type and place it in hostMsg parameters
@@ -82,9 +82,22 @@ void log_peel_params(hostMsg *hmsg, uint32_t* cnt, const P1& p1, Param& ... para
     return;
 }
 
+//! to_raw_binary: Converts any parameter type into raw binary that can be packed into the payload
+// (type information is stored in the unparameterised string at the host end)
+//template<typename T>
+//uint32_t to_raw_binary(const T& t){
+//  return *((uint32_t*)&t); 
+//}
+//
+////! unpeel_all_params: unpeels all the parameters of the log function call and stores them in a vector
+//template<typename ... Param>
+//std::vector<uint32_t> unpeel_all_params(const Param& ... param){
+//  return {to_raw_binary(param)...};
+//} 
+
 //! The log message call
 template<typename ... Param>
-void softswitch_handler_log(int level, const char *msg, const Param& ... param){
+void softswitch_handler_log_impl(int level, const char *msg, const Param& ... param){
 
   PThreadContext *ctxt=softswitch_getCtxt();
   assert(ctxt->currentHandlerType!=0); // can't call handler log when not in a handler
@@ -106,8 +119,13 @@ void softswitch_handler_log(int level, const char *msg, const Param& ... param){
   // peel off the parameters from the variadic
   uint32_t param_cnt = 1;
   log_peel_params(&hmsg, &param_cnt, param...);
-
   assert(param_cnt <= HOST_MSG_PAYLOAD);
+
+  //std::vector<uint32_t> params = unpeel_all_params(param...);
+  //assert(params.size() <= HOST_MSG_PAYLOAD);
+  //for(uint32_t i=0; i<params.size(); i++) {
+  //  hmsg.payload[i] = params[i]; 
+  //}
 
   // push the message onto the hostMsgBuffer
   hostMsgBufferPush(&hmsg);
@@ -120,7 +138,7 @@ void softswitch_handler_log(int level, const char *msg, const Param& ... param){
 //! The softswith log messaging (this is for debugging, it uses the slower UART channel as otherwise a deadlock might occur)
 // extern "C" void softswitch_softswitch_log_impl(int level, const char *msg, ...)
 template<typename ... Param>
-void softswitch_softswitch_log(int level, const char *msg, const Param& ... param){
+void softswitch_softswitch_log_impl(int level, const char *msg, const Param& ... param){
 
   PThreadContext *ctxt=softswitch_getCtxt();
   assert(ctxt->currentHandlerType!=0); // can't call handler log when not in a handler
@@ -142,8 +160,13 @@ void softswitch_softswitch_log(int level, const char *msg, const Param& ... para
   // peel off the parameters from the variadic
   uint32_t param_cnt = 1;
   log_peel_params(&hmsg, &param_cnt, param...);
+  assert(params_cnt <= HOST_MSG_PAYLOAD);
 
-  assert(param_cnt <= HOST_MSG_PAYLOAD);
+  //std::vector<uint32_t> params = unpeel_all_params(param...);
+  //assert(params.size() <= HOST_MSG_PAYLOAD);
+  //for(uint32_t i=0; i<params.size(); i++) {
+  //  hmsg.payload[i] = params[i]; 
+  //}
 
   // This does not go through the buffer as it could cause deadlock
   directHostMessageSlowSend(&hmsg);
